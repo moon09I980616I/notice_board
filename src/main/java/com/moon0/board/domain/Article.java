b0f92a54306bb4b0de72d8558e71bdedda623d94
김문영 <zxcv199698@naver.com>
2022-10-28 16:46:09 +0900

    #4 도메인 클래스 설계(게시글, 댓글) 도메인 정보 설계 기초적인 게시글과 댓글 표현 DB 연동을 위한 추가적인 표현은 후에 업데이트

---
+++ src/main/java/com/moon0/board/domain/Article.java
@@ -0,0 +1,81 @@
+package com.moon0.board.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    /*domain*/
+    private Long id;
+    private String title; //제목
+    private String content; //본문
+    private String hashtag; //해시태그
+
+    /*metadata*/
+    private LocalDateTime createdAt; //생성일시
+    private String createdBy; //생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy; //수정자
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getHashtag() {
+        return hashtag;
+    }
+
+    public void setHashtag(String hashtag) {
+        this.hashtag = hashtag;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
---
+++ src/main/java/com/moon0/board/domain/ArticleComment.java
@@ -0,0 +1,72 @@
+package com.moon0.board.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    /*domain*/
+    private Long id;
+    private Article article; //게시글 (id)
+    private String content; //본문
+
+    /*metadata*/
+    private LocalDateTime createdAt; //생성일시
+    private String createdBy; //생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy; //수정자
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+}
