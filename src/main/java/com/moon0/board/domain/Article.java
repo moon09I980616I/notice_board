fbb7a841dca183009e0104d42352a280b95ae7fa
김문영 <zxcv199698@naver.com>
2022-11-04 10:34:34 +0900

    #6 데이터베이스 접근로직 테스트 정의

--- src/main/java/com/moon0/board/domain/Article.java
+++ src/main/java/com/moon0/board/domain/Article.java
@@ -1,81 +1,72 @@
 package com.moon0.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList="title"),
+        @Index(columnList="hashtag"),
+        @Index(columnList="createdAt"),
+        @Index(columnList="createdBy"),
+})
+@Entity
 public class Article {
-    /*domain*/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; //제목
-    private String content; //본문
-    private String hashtag; //해시태그
 
-    /*metadata*/
-    private LocalDateTime createdAt; //생성일시
-    private String createdBy; //생성자
-    private LocalDateTime modifiedAt; //수정일시
-    private String modifiedBy; //수정자
+    //수정 가능한 것만 setter 처리
+    @Setter @Column(nullable = false) private String title; //제목
+    @Setter @Column(nullable = false, length=10000) private String content; //본문
 
-    public Long getId() {
-        return id;
-    }
+    @Setter private String hashtag; //해시태그 null
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 중복을 허용하지 않고 리스트를 보여주겠다.
 
-    public String getTitle() {
-        return title;
-    }
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
+    @CreatedBy @Column(nullable = false, length=100) private String createdBy; //생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
+    @LastModifiedBy @Column(nullable = false, length=100)private String modifiedBy; //수정자
 
-    public void setTitle(String title) {
+    /*jpa entity 는 기본 생성자를 가지고 있어야 하기 때문에 생성을 해주며, 평소에는 오픈하지 않을 것이기 때문에 public 이 아닌 protected 를 사용 */
+    protected Article(){}
+
+    private Article(String title, String content, String hashtag) {
         this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
-    }
-
-    public String getHashtag() {
-        return hashtag;
-    }
-
-    public void setHashtag(String hashtag) {
         this.hashtag = hashtag;
     }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
+    /*domain(Article) 생성 시 필요로 하는 값들*/
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
     }
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDateTime getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(LocalDateTime modifiedAt) {
-        this.modifiedAt = modifiedAt;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
