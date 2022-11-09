d2bcc2495d5fd255b4d957a378c0624c5d014fa8
main <zxcv199698@naver.com>
2022-11-09 16:01:33 +0900

    #8 - 회원 계정 도메인 구현
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

---
+++ src/main/java/com/moon0/board/repository/UserAccountRepository.java
@@ -0,0 +1,8 @@
+package com.moon0.board.repository;
+
+import com.moon0.board.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+
+}
